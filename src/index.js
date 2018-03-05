6607fc7ef2f9cb7a503ec969338d0edf382d76d1
tatyana-andreichyk <tatyana.andreichyk@gmail.com>
2018-03-05 16:42:13 +0300

    zeros/feature: implemented solution search of zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zerosCountRes = 0;
+  while (number > 0) {
+    number = (number / 5);
+    zerosCountRes = zerosCountRes + Math.floor(number);
+  }
+  return zerosCountRes;
 }
